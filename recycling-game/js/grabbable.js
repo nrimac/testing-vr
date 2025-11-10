22f2f66149ddf930bea81080917b34efa597d1ef
nrimac <nrimac00@gmail.com>
2025-11-10 10:00:03 +0000

    Fix grab 3

--- recycling-game/js/grabbable.js
+++ recycling-game/js/grabbable.js
@@ -3,6 +3,10 @@ AFRAME.registerComponent("grabbable", {
     this.grabbing = false;
     this.grabbingHand = null;
 
+    if (this.el.components.sleepy) {
+      this.el.components.sleepy.wakeUp();
+    }
+
     // Bind event handlers to this component instance
     this.onTriggerDown = this.onTriggerDown.bind(this);
     this.onTriggerUp = this.onTriggerUp.bind(this);
@@ -12,66 +16,49 @@ AFRAME.registerComponent("grabbable", {
   },
 
   onTriggerDown: function (evt) {
-    if (this.grabbing) {
-      return;
-    }
+    // Prevent grabbing if already held
+    if (this.grabbing) { return; }
 
+    // Get the controller that triggered the event
     const hand = evt.detail.cursorEl;
     this.grabbing = true;
     this.grabbingHand = hand;
-
     if (this.el.components.sleepy) {
-      this.el.components.sleepy.wakeUp();
+        this.el.components.sleepy.wakeUp();
     }
 
+    // Temporarily disable physics by removing the dynamic-body while grabbing
     this.el.removeAttribute("dynamic-body");
+
+    // Attach the object to the grabbing hand
     this.grabbingHand.object3D.attach(this.el.object3D);
+
+    // Listen for the trigger release on the grabbing hand
     this.grabbingHand.addEventListener("triggerup", this.onTriggerUp);
   },
 
   onTriggerUp: function (evt) {
-    if (!this.grabbing || !this.grabbingHand) {
-      return;
-    }
-
+    // Ensure we are in a grabbing state and the event is from the correct hand
+    if (!this.grabbing || !this.grabbingHand) { return; }
+    
     const sceneEl = this.el.sceneEl;
     const worldPosition = new THREE.Vector3();
     const worldRotation = new THREE.Quaternion();
-    const el = this.el; // Store a reference to the element for use in callbacks
 
     // Get the object's current world position and rotation
-    el.object3D.getWorldPosition(worldPosition);
-    el.object3D.getWorldQuaternion(worldRotation);
+    this.el.object3D.getWorldPosition(worldPosition);
+    this.el.object3D.getWorldQuaternion(worldRotation);
 
     // Detach from the hand and re-attach to the scene
-    sceneEl.object3D.attach(el.object3D);
-
+    sceneEl.object3D.attach(this.el.object3D);
+    
     // Set the object's position and rotation in the world
-    el.object3D.position.copy(worldPosition);
-    el.object3D.quaternion.copy(worldRotation);
+    this.el.object3D.position.copy(worldPosition);
+    this.el.object3D.quaternion.copy(worldRotation);
 
-    // --- Start of the definitive fix ---
-    // Defer re-adding the physics body to the next tick to ensure the scene graph is updated.
-    setTimeout(() => {
-      // Re-add the dynamic-body component to re-enable physics.
-      el.setAttribute("dynamic-body", { mass: 0.2 });
-
-      // The physics body is not created instantly. We must wait for the 'body-loaded' event.
-      el.addEventListener(
-        "body-loaded",
-        () => {
-          // Now that the physics body exists, reset its velocities.
-          // This prevents it from flying off with the hand's last velocity.
-          if (el.body) {
-            el.body.velocity.set(0, 0, 0);
-            el.body.angularVelocity.set(0, 0, 0);
-          }
-        },
-        { once: true } // The listener will automatically remove itself after firing once.
-      );
-    }, 0);
-    // --- End of the definitive fix ---
-
+    // Re-enable physics by adding the dynamic-body component back
+    this.el.setAttribute("dynamic-body", { mass: 0.2 });
+    
     // Clean up the listener on the hand
     this.grabbingHand.removeEventListener("triggerup", this.onTriggerUp);
 
@@ -79,12 +66,12 @@ AFRAME.registerComponent("grabbable", {
     this.grabbing = false;
     this.grabbingHand = null;
   },
-
+  
   // Clean up listeners if the entity is removed
-  remove: function () {
+  remove: function() {
     this.el.removeEventListener("mousedown", this.onTriggerDown);
     if (this.grabbingHand) {
       this.grabbingHand.removeEventListener("triggerup", this.onTriggerUp);
     }
-  },
+  }
 });
