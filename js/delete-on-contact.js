94d29b87ef6c9935317762825887441b5b6c20ef
nrimac <nrimac00@gmail.com>
2025-12-09 15:09:18 +0000

    Grab works, fix rest pls omg

--- js/delete-on-contact.js
+++ js/delete-on-contact.js
@@ -1,16 +1,20 @@
 AFRAME.registerComponent('delete-on-contact', {
   init: function () {
-    // Listen for the AABB Collider 'hitstart' event
-    this.el.addEventListener('hitstart', (evt) => {
+    // Listen for the physics system's collision event
+    this.el.addEventListener('collide', (e) => {
       
-      // evt.detail.intersectedEls contains all objects touching the bin
-      evt.detail.intersectedEls.forEach(hitEl => {
+      // e.detail.body refers to the physics body of the object that hit the bin
+      // We access the A-Frame element attached to that body via .el
+      const hitEl = e.detail.body ? e.detail.body.el : null;
+
+      // Check if the element exists and has the 'grabbable' class
+      if (hitEl && hitEl.classList.contains('grabbable')) {
         
-        // If the object is trash (grabbable), remove it
-        if (hitEl.classList.contains('grabbable')) {
+        // Remove the element from the scene if it is still attached
+        if (hitEl.parentNode) {
           hitEl.parentNode.removeChild(hitEl);
         }
-      });
+      }
     });
   }
 });
